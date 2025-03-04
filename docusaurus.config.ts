2e6e8f8434a79ac82a4b8b96740d3656e00977ab
Huimin Chang <huiminchang@mail.bnu.edu.cn>
2025-03-04 14:22:28 +0800

    update sidebar positions in FAQ and resources, and correct edit URL in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tiangong-lca/tidas/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -52,7 +52,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tiangong-lca/tidas/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
